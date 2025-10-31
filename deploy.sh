9db28304eab052d4377e1d9fc7def0d71532e84a
davidfastovich <davidfastovich@gmail.com>
2025-10-31 15:08:28 -0400

    trying a fix for the username issue

--- deploy.sh
+++ deploy.sh
@@ -29,8 +29,8 @@ cp gh-pages/index.html gh-pages/404.html
 
 # Commit and push changes
 cd gh-pages
-git config user.name "github-actions[bot]"
-git config user.email "github-actions[bot]@users.noreply.github.com"
+git config --local user.email "github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
 git add --all
 git commit -m "Deploy website"
 git push --force origin $TARGET_BRANCH
