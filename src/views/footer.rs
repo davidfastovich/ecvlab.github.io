1c2ca637cf2654eb7103c0b504a5d6efbc1b5060
davidfastovich <davidfastovich@gmail.com>
2025-10-31 13:46:19 -0400

    init website commit

---
+++ src/views/footer.rs
@@ -0,0 +1,13 @@
+use crate::Route;
+use dioxus::prelude::*;
+
+#[component]
+pub fn Footer() -> Element {
+    rsx! {
+        div{ class: "max-w-4xl mx-auto p-6 gap-8",
+            
+            hr { class: "border-gray-300 mb-10" }
+            
+        }
+    }
+}
